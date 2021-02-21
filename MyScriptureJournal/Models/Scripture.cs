0e8a11822ac98f28dee8a45d15da7d9634414dee
Garrett Matthews <garrett.r.matthews@gmail.com>
2021-02-20 17:08:15 -0700

    Add project files.

---
+++ MyScriptureJournal/Models/Scripture.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+
+namespace MyScriptureJournal.Models
+{
+    public class Scripture
+    {
+        public int ID { get; set; }
+        public string Citation { get; set; }
+        [Display(Name = "Release Date")]
+        [DataType(DataType.Date)]
+        public DateTime ReleaseDate { get; set; }
+        public string Book { get; set; }
+        public string Note { get; set; }
+    }
+}
