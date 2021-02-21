cf635a58c215124182d6f9ca7ff6ce43299235f7
Garrett Matthews <garrett.r.matthews@gmail.com>
2021-02-20 18:06:37 -0700

    initial commit

--- MyScriptureJournal/Startup.cs
+++ MyScriptureJournal/Startup.cs
@@ -29,6 +29,7 @@ namespace MyScriptureJournal
 
             services.AddDbContext<MyScriptureJournalContext>(options =>
                     options.UseSqlServer(Configuration.GetConnectionString("MyScriptureJournalContext")));
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
